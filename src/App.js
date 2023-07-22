de121c06d382e57abc3d5c1aa0850c37b1867522
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-07-22 15:03:24 +0400

    created a list of posts, implemented adding and deleting posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles/App.css";
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+  //получаем пост из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
   return (
     <div className="App">
-      <p>template</p>
+      <PostForm create={createPost} />
+      {posts.length !== 0 ? (
+        <PostList remove={removePost} posts={posts} title="Cписок постов 1" />
+      ) : (
+        <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
+      )}
     </div>
   );
 }
---
+++ src/components/PostList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PostItem from "./PostItem";
+
+const PostList = ({posts, title, remove}) => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>{title}</h1>
+      {posts.map((post, index) => {
+        return <PostItem remove={remove} number={index + 1} key={post.id} post={post} />;
+      })}
+    </div>
+  );
+};
+
+export default PostList;
