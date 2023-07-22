878093b887e584b2f214505c157cff03aa9db9c1
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-07-22 17:28:33 +0400

    implemented a sorting mechanism

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/Select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,6 +10,7 @@ function App() {
     { id: 2, title: "Javascript 2", body: "Description" },
     { id: 3, title: "Javascript 3", body: "Description" },
   ]);
+  const [selectedSort, setSelectedSort] = useState("");
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   };
@@ -16,14 +18,36 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
+  const sortPost = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
 
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{ margin: "15px 0" }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPost}
+          defaultValue="Cортировка"
+          options={[
+            {
+              value: "title",
+              name: "По названию",
+            },
+            {
+              value: "body",
+              name: "По описанию",
+            },
+          ]}
+        />
+      </div>
       {posts.length !== 0 ? (
         <PostList remove={removePost} posts={posts} title="Cписок постов 1" />
       ) : (
-        <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
+        <h1 style={{ textAlign: "center" }}>Посты не найдены!</h1>
       )}
     </div>
   );
