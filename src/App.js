614518b581b11615c77d686945c62641061e8d06
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-07-23 21:12:44 +0400

    made pagination to display posts page by page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { useEffect, useState } from "react";
+import { usePosts } from "./Hooks/usePosts";
+import { useFetching } from "./Hooks/useFetching";
+import { getPageCount, getPagesArray } from "./Utils/Pages";
+import PostService from "./API/PostService";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostsFilter from "./components/PostsFilter";
 import MyModal from "./components/UI/Modal/MyModal";
 import MyButton from "./components/UI/Button/MyButton";
-import { usePosts } from "./Hooks/usePosts";
-import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
-import { useFetching } from "./Hooks/useFetching";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
-  });
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
 
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  let pagesArray = getPagesArray(totalPages);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
+  });
+  console.log(totalPages);
+  console.log(postError);
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [page]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -32,6 +43,11 @@ function App() {
   //получаем пост из дочернего компонента
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  //пагинация
+  const changePage = (page) => {
+    setPage(page);  
   };
 
   return (
@@ -58,6 +74,19 @@ function App() {
           title="Cписок постов 1"
         />
       )}
+      <div className="page__wrapper">
+        {pagesArray.map((p) => {
+          return (
+            <span
+              onClick={() => changePage(p)}
+              key={p}
+              className={page === p ? "page page__current" : "page"}
+            >
+              {p}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
