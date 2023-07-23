1d3d328a68076aaa59a796e404bc66cf2af77452
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-07-23 13:40:03 +0400

    made a separate hook that provides loading indication processing and error processing for receiving data

--- src/App.js
+++ src/App.js
@@ -8,26 +8,21 @@ import MyButton from "./components/UI/Button/MyButton";
 import { usePosts } from "./Hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./Hooks/useFetching";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
   }, []);
-
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    }, 1000);
-  }
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -49,8 +44,11 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostsFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>Произошла ошибка...${postError}</h1>}
       {isPostsLoading ? (
-        <div style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+        >
           <Loader />
         </div>
       ) : (
