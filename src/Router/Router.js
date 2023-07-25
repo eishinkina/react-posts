64acf396a35ebdc2d13db5b0ca28649d5cd7be34
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-07-25 18:34:31 +0400

    implemented useContext. global data. User authorization

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,10 +1,13 @@
-
 import About from "./../Pages/About";
 import Posts from "./../Pages/Posts";
-import PostsIdPage from "./../Pages/PostIdPage"
+import PostsIdPage from "./../Pages/PostIdPage";
+import Login from "../Pages/Login";
 
-export const routes = [
-    {path: '/about', element: <About />, exact: true},
-    {path: '/posts', element: <Posts />, exact: true},
-    {path: '/posts/:id', element: <PostsIdPage />, exact: true}
-]
+export const privateRoutes = [
+  { path: "/about", element: <About />, exact: true },
+  { path: "/posts", element: <Posts />, exact: true },
+  { path: "/posts/:id", element: <PostsIdPage />, exact: true },
+];
+export const publicRoutes = [
+  { path: "/login", element: <Login />},
+];
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,18 +1,29 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { routes } from "../Router/Router";
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { privateRoutes, publicRoutes } from "../Router/Router";
+import { AuthContext } from "../Context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-  return (
-    <div>
-      <Routes>
-        {routes.map((route, index) => {
-          return (
-            <Route key={index} element={route.element} path={route.path} />
-          );
-        })}
-      </Routes>
-    </div>
+   const{isAuth, isLoading} = useContext(AuthContext);
+   console.log(isAuth)
+   if(isLoading){
+    return <Loader />
+   }
+  return isAuth ? (
+    <Routes>
+      {privateRoutes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+      <Route path="*" element={<Navigate to="/posts" replace />} />
+    </Routes>
+  ) : (
+    <Routes>
+      {publicRoutes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
 };
 
